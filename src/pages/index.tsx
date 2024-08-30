4f0204254609169ee0353b9f5f98d8ef4108d9aa
Sandeshstha0 <sandeshstha519@gmail.com>
2024-08-30 18:32:03 +0545

    first-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Inter } from "next/font/google"
+
+export default function Home() {
+  return (
+    <div className="">
+      hwllp
+      <div className=" ">
+        <div className="my-2 mt-3">
+       
+        </div>
+
+        <div className="right mx-6 md:w-4/5">
+          <h1 className=" ">
+            Hello, asd !
+          </h1>
+          <h2 className="mt-3 sm:mt-2 text-left font-normal">
+            Welcome to your College Profile! Your personalized gateway to academic success and campus life. This is your hub for academic resources, announcements, and important information.
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
