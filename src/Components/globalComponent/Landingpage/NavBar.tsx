2ea2bcb5e8797cc148c3db8d5b1b72c399a527ba
Sandeshstha0 <sandeshstha519@gmail.com>
2024-09-08 06:44:03 +0545

    foldrer change

---
+++ src/Components/globalComponent/Landingpage/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+    <div className='flex justify-between p-6'>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+    </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import NavBar from "@/Components/globalComponent/Landingpage/NavBar";
 import DoYouKnow from "@/Components/PageComponent/Landingpage/Home/DoYouKnow";
 import HeroSection from "@/Components/PageComponent/Landingpage/Home/HeroSection";
 import SecondSection from "@/Components/PageComponent/Landingpage/Home/SecondSection";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
       <div className=" bg-primary overflow-y-hidden">
+        <NavBar/>
         <HeroSection />
         <SecondSection />
         <DoYouKnow />
