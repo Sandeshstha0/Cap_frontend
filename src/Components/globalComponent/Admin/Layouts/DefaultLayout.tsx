a3905577bbfd15b37eb19d56c0e95a496f213c4b
Sandeshstha0 <sandeshstha519@gmail.com>
2024-09-11 16:46:58 +0545

    jnki

---
+++ src/Components/globalComponent/Admin/Layouts/DefaultLayout.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useState } from "react";
+import Header from "../Header";
+import Sidebar from "../Sidebar";
+
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      {/* Page Wrapper */}
+      <div className="flex flex-col h-screen overflow-hidden">
+        {/* Header at the Top */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        {/* Main Content Area with Sidebar */}
+        <div className="flex flex-1 overflow-hidden">
+          {/* Sidebar */}
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+          {/* Main Content */}
+          <div className={`relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden transition ${sidebarOpen?'blur':''}`}>
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-3 md:p-6 2xl:p-6">
+                {children}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/admin/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/Components/globalComponent/Admin/Layouts/DefaultLayout'
+import React from 'react'
+
+export default function hello() {
+  return (
+    <DefaultLayout>
+    <div>hello</div>
+    </DefaultLayout>
+  )
+}
