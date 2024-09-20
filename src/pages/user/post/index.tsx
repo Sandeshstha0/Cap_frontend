2136aa8fe8534f11f60cf06c35595dfeb56950cf
mmelu0599 <mmelinastha18@gmail.com>
2024-09-20 06:52:45 +0545

    ss

---
+++ src/pages/user/post/index.tsx
@@ -0,0 +1,23 @@
+import DefaultLayout from "@/Components/globalComponent/Admin/Layouts/DefaultLayout";
+import React from "react";
+
+export default function index() {
+  return (
+    <DefaultLayout>
+    <div>
+      <div>
+        {/* Page Heading */}
+        <div className="bg-white text-left text-primary font-normal  px-15 py-6 ">
+          <b>Hello, Sachina!</b>
+          <p>
+            Welcome to the Budget Expert, here we manage you daily Expense and
+            Projects
+          </p>
+         </div>
+         
+      </div>
+    </div>
+    </DefaultLayout>
+    
+  );
+}
